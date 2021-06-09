8f5b51d14661d7cd6d6b3c208091ca3694b3be44
Juha Ritakoski <juha.ritakoski@hotmail.com>
2021-06-09 21:13:52 +0300

    Added red and blue emojis
    removed old emoji from testing

--- functions/init_emojis.js
+++ functions/init_emojis.js
@@ -3,9 +3,9 @@ const Discord = require('discord.js')
 module.exports = async (bot) => {
     let emojis = {}
 
-    emojis.ecto = "🎲"
-    const ecto_emoji = bot.emojis.cache.find(emoji => emoji.name === 'ecto')
-    if (ecto_emoji) emojis.ecto = ecto_emoji
+    emojis.red = "🔴"
+
+    emojis.blue = "🔵"
     
     emojis.blank = "✴️"
     const blank_emoji = bot.emojis.cache.find(emoji => emoji.name === 'blank')
