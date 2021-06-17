e9f81c361382bca5cca8f313df61e737ae2f949a
Juha Ritakoski <juha.ritakoski@hotmail.com>
2021-06-17 22:47:16 +0300

    add extended base for button handling

--- functions/button_handler.js
+++ functions/button_handler.js
@@ -1,9 +1,25 @@
 module.exports = async (bot, message, button_id, user) => {
     let game = bot.games[message.id]
 
-    let place = game.board[button_id]
-    game.chosen = button_id
+    if (game.players[game.turn].id != user.id) {
+        console.log("users didn't match")
+        return
+    }
 
-    
-    console.log(bot.games[message.id].chosen)
+    let buttons = {}
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 5; j++) {
+            let btn = sent.components[i].components[j]
+            buttons[btn.custom_id] = btn
+        }
+    }
+
+    if (game.chosen) {
+
+        // at end
+        game.chosen = ""
+        game.turn = (game.turn + 1) % 2
+    } else {
+        game.chosen = button_id
+    }
 }
